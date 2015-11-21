f8173ca9bc6c452886ef8c03b7f146fa680a6dcc
Niklas Alanko <alanko.jarno@gmail.com>
2015-11-21 19:06:29 +0000

    Test also patterns longer than input

--- tests.cpp
+++ tests.cpp
@@ -63,16 +63,18 @@ int main(int argc, char** argv){
     cout << "Suffix array correct for all substrings of length <= " << SA_max << endl;
     
     int kmp_max = 10;
-    for(string s : all_binary_substrings_up_to(kmp_max)){
-        for(string p : all_binary_substrings_up_to(s.size())){
+    vector<string> kmp_all = all_binary_substrings_up_to(kmp_max);
+    for(string s : kmp_all){
+        for(string p : kmp_all){
             assert(naive_pattern_matching(p,s) == kmp(p,s));
         }
     }
     cout << "KMP correct for all substrings of length <= " << kmp_max << endl;
 
     int rabin_karp_max = 10;
-    for(string s : all_binary_substrings_up_to(rabin_karp_max)){
-        for(string p : all_binary_substrings_up_to(s.size())){
+    vector<string> rabin_karp_all = all_binary_substrings_up_to(rabin_karp_max);
+    for(string s : rabin_karp_all){
+        for(string p : rabin_karp_all){
             assert(naive_pattern_matching(p,s) == rabin_karp(p,s));
         }
     }
